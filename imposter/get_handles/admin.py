f325f0ae1f00258445c852ab9d947485d0c5c147
wjleddy <williamjleddy@gmail.com>
2019-12-16 19:46:08 -0800

    Restructure directories to make heroku happy

--- imposter/get_handles/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import BaseHandle, SimilarHandles
-
-admin.site.register(BaseHandle)
-admin.site.register(SimilarHandles)
--- imposter/get_handles/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GetHandlesConfig(AppConfig):
-    name = 'get_handles'
