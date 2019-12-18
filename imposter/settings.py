f26889f110722cd82abe1c3957659ee9953a367c
wjleddy <williamjleddy@gmail.com>
2019-12-18 12:24:25 -0800

    Change STATICFILES_STORAGE back to whitenoise in settings.py

--- imposter/settings.py
+++ imposter/settings.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
